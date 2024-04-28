7ed422eea5c6b99167eb33084e7020a06f18d58b
thobenayann <thobena.yann@orange.fr>
2024-04-28 21:17:07 +0200

    logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,14 @@
+import { PiggyBank } from 'lucide-react';
+
+function Logo() {
+    return (
+        <a href='/' className='flex items-center gap-2'>
+            <PiggyBank className='stroke h-11 w-11 stroke-purple-500' />
+            <p className='bg-gradient-to-r from-purple-400 to-violet-600 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent'>
+                MyBudgetTracker
+            </p>
+        </a>
+    );
+}
+
+export default Logo;
