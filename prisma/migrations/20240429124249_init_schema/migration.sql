fcfaaf8b142b1d9e4c9eb3ae3c8bb22ae278853a
thobenayann <thobena.yann@orange.fr>
2024-04-29 14:50:13 +0200

    init prisma with sqlite

---
+++ prisma/migrations/20240429124249_init_schema/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "UserSettings" (
+    "userId" TEXT NOT NULL PRIMARY KEY,
+    "currency" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "incon" TEXT NOT NULL,
+    "type" TEXT NOT NULL DEFAULT 'income'
+);
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" REAL NOT NULL,
+    "description" TEXT,
+    "date" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "categoryIcon" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "MonthHistory" (
+    "userId" TEXT NOT NULL,
+    "day" INTEGER NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "income" REAL NOT NULL,
+    "expense" REAL NOT NULL,
+
+    PRIMARY KEY ("userId", "day", "month", "year")
+);
+
+-- CreateTable
+CREATE TABLE "YearHistory" (
+    "userId" TEXT NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "income" REAL NOT NULL,
+    "expense" REAL NOT NULL,
+
+    PRIMARY KEY ("userId", "month", "year")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_userId_type_key" ON "Category"("name", "userId", "type");
