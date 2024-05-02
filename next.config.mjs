aabfa903fec87b38c5718ef512b429229d943e1d
thobenayann <thobena.yann@orange.fr>
2024-05-02 19:51:33 +0200

    modify next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const path = require('path');
+
+const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, './'),
+        };
+
+        return config;
+    },
+};
+
+module.exports = nextConfig;
